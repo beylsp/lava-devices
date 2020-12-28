71f3c67d191cc2024d2a205f3291883106cbe3e9
Patrik Beyls <patrik.beyls@worldline.com>
2020-12-28 12:26:09 +0100

    generate fastboot vendor/product id

--- make-board-files.py
+++ make-board-files.py
@@ -6,17 +6,30 @@ import sys
 import common
 
 
-def generate_device_file(devices_dir, board):
+def get_dev_type_info(types, name):
+    for t in types:
+        if name == t["name"]:
+            return t
+    return None
+
+
+def generate_device_file(devices_dir, board, types):
     board_name = board["name"]
     dev_type = board["type"]
-    fserial = board["fastboot_serial_number"]
+    fbserial = board["fastboot_serial_number"]
+    dev_type_info = get_dev_type_info(types, dev_type)
+    vendor = dev_type_info["vendor_id"]
+    fbvendor = dev_type_info["fastboot_vendor_id"]
+    product = dev_type_info["product_id"]
+    fbproduct = dev_type_info["fastboot_product_id"]
     board_file = os.path.join(devices_dir, "%s.jinja2" % board_name)
     with open(board_file, 'w') as board_jinja:
         print("Processing: %s (%s)" % (board_name, board_file))
         board_jinja.write("{%% extends '%s.jinja2' %%}\n" % dev_type)
-        board_jinja.write("{%% set fastboot_serial_number = '%s' %%}\n" % fserial)
+        board_jinja.write("{%% set fastboot_serial_number = '%s' %%}\n" % fbserial)
         board_jinja.write("{% set adb_serial_number = fastboot_serial_number %}\n")
-        board_jinja.write("{% set static_info = [{'board_id': fastboot_serial_number}] %}\n")
+        board_jinja.write("{%% set device_info = [{'board_id': fastboot_serial_number, 'vendor_id': '%s', 'product_id': '%s'}, {'board_id': fastboot_serial_number, 'vendor_id': '%s', 'product_id': '%s'}] %%}\n" % (vendor, product, fbvendor, fbproduct))
+        board_jinja.write("{%% set static_info = [{'board_id': fastboot_serial_number, 'vendor_id': '%s', 'product_id': '%s'}] %%}\n" % (vendor, product))
 
 
 def parse_cli():
@@ -37,9 +50,10 @@ def parse_cli():
 
 def main():
     cli = parse_cli()
+    types = common.load_types(cli.definition)
     boards = common.load_boards(cli.definition)
     for board in boards:
-        generate_device_file(cli.devices_dir, board)
+        generate_device_file(cli.devices_dir, board, types)
 
 
 if __name__ == '__main__':
