6ad5d9ad3ef5c59a8fc45d80c7a70f921d0cfd13
Patrik Beyls <patrik.beyls@worldline.com>
2020-12-29 16:17:33 +0100

    add wait_for_worker()

--- board-setup-helper.py
+++ board-setup-helper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import argparse
+import datetime as dt
 import os
 import socket
 import sys
@@ -23,6 +24,32 @@ class LavaXmlRpc(object):
     def __exit__(self, exc_type, exc_value, exc_traceback):
         pass
 
+    def worker_ready(self, worker_hostname):
+        try:
+            workers = self.xmlrpc.scheduler.workers.list()
+            return worker_hostname in workers
+        except socket.gaierror as err:
+            return False
+
+    def wait_for_worker(self, worker_hostname, timeout):
+        start = dt.datetime.now()
+        if timeout == 0:
+            wait_until = 0
+        else:
+            wait_until = start + dt.timedelta(seconds=timeout)
+        ready = False
+        timed_out = True
+        current = start
+        while current < wait_until:
+            if worker_ready(worker_hostname):
+                ready = True
+                timed_out = False
+                break
+            current = dt.datetime.now()
+        if timed_out:
+            print("timeout occurred after waiting %s seconds for worker '%s'" % (timeout, worker_hostname))
+        return ready
+
     def add_lava_device_types(self, device_types, retries=1):
         print("Adding device types")
         for device_type in device_types:
@@ -44,14 +71,22 @@ class LavaXmlRpc(object):
             if failed:
                 raise Exception(errm)
 
-    def add_lava_devices(self, devices, retries=1):
+    def add_lava_devices(self, devices, retries=1, timeout=60, strict=True):
         print("Adding lava devices")
         for device in devices:
             hostname = device["name"]
             type_name = device["type"]
             worker_hostname = device["worker"]
-            description = device["description"]
+
             print("- %s (%s) on %s" % (hostname, type_name, worker_hostname))
+            running = self.wait_for_worker(worker_hostname, timeout)
+            if strict and not running:
+                print("strict mode, abort")
+                sys.exit(1)
+            else:
+                continue
+
+            description = device["description"]
             retry_count = 0
             failed = True
             while retry_count <= retries:
@@ -95,6 +130,14 @@ def parse_cli():
         "--retries", type=int, default=5,
         help="XMLRPC connection retries"
     )
+    parser.add_argument(
+        "--worker-timeout", type=int, default=120,
+        help="time in seconds to wait for worker, zero for no timeout"
+    )
+    parser.add_argument(
+        "-s", "--strict", action="store_true",
+        help="fail if worker is not running"
+    )
 
     return parser.parse_args(sys.argv[1:])
 
@@ -106,7 +149,7 @@ def main():
             device_types = common.load_types(cli.definition)
             xmlrpc.add_lava_device_types(device_types, cli.retries)
             devices = common.load_boards(cli.definition)
-            xmlrpc.add_lava_devices(devices, cli.retries)
+            xmlrpc.add_lava_devices(devices, cli.retries, cli.worker_timeout, cli.strict)
     except Exception as err:
         print("XMLRPC: %s" % err)
         sys.exit(1)
