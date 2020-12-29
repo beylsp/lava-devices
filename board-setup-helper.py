b2ee68c8048e556827963c8ca5c75efe340f7c41
Patrik Beyls <patrik.beyls@worldline.com>
2020-12-29 11:43:25 +0100

    board-setup-helper: add retry mechanism

--- board-setup-helper.py
+++ board-setup-helper.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import argparse
 import os
+import socket
 import sys
+import time
 import xmlrpc.client
 
 import common
@@ -21,15 +23,28 @@ class LavaXmlRpc(object):
     def __exit__(self, exc_type, exc_value, exc_traceback):
         pass
 
-    def add_lava_device_types(self, device_types):
+    def add_lava_device_types(self, device_types, retries=1):
         print("Adding device types")
         for device_type in device_types:
             name = device_type["name"]
             description = device_type["description"]
             print("- %s" %  name)
-            self.xmlrpc.scheduler.device_types.add(name, description, True, False, 0, "jobs")
+            retry_count = 0
+            failed = True
+            while retry_count <= retries:
+                try:
+                    self.xmlrpc.scheduler.device_types.add(name, description, True, False, 0, "jobs")
+                except socket.gaierror as err:
+                    errm = "%s (after %s retries)" % (err, retry_count)
+                    retry_count += 1
+                    time.sleep(5)
+                else:
+                    failed = False
+                    break
+            if failed:
+                raise Exception(errm)
 
-    def add_lava_devices(self, devices):
+    def add_lava_devices(self, devices, retries=1):
         print("Adding lava devices")
         for device in devices:
             hostname = device["name"]
@@ -37,10 +52,23 @@ class LavaXmlRpc(object):
             worker_hostname = device["worker"]
             description = device["description"]
             print("- %s (%s) on %s" % (hostname, type_name, worker_hostname))
-            self.xmlrpc.scheduler.devices.add(hostname, type_name, worker_hostname, description)
-            tags = device["tags"]
-            for name in tags:
-                self.xmlrpc.scheduler.devices.tags.add(hostname, name)
+            retry_count = 0
+            failed = True
+            while retry_count <= retries:
+                try:
+                    self.xmlrpc.scheduler.devices.add(hostname, type_name, worker_hostname, description)
+                    tags = device["tags"]
+                    for name in tags:
+                        self.xmlrpc.scheduler.devices.tags.add(hostname, name)
+                except socket.gaierror as err:
+                    errm = "%s (after %s retries)" % (err, retry_count)
+                    retry_count += 1
+                    time.sleep(5)
+                else:
+                    failed = False
+                    break
+            if failed:
+                raise Exception(errm)
 
 
 def parse_cli():
@@ -63,6 +91,10 @@ def parse_cli():
         "-u", "--lava-user", dest="user", required=True,
         help="lava user name"
     )
+    parser.add_argument(
+        "--retries", type=int, default=5,
+        help="XMLRPC connection retries"
+    )
 
     return parser.parse_args(sys.argv[1:])
 
@@ -72,9 +104,9 @@ def main():
     try:
         with LavaXmlRpc(cli.host, cli.user, cli.token) as xmlrpc:
             device_types = common.load_types(cli.definition)
-            xmlrpc.add_lava_device_types(device_types)
+            xmlrpc.add_lava_device_types(device_types, cli.retries)
             devices = common.load_boards(cli.definition)
-            xmlrpc.add_lava_devices(devices)
+            xmlrpc.add_lava_devices(devices, cli.retries)
     except Exception as err:
         print("XMLRPC: %s" % err)
         sys.exit(1)
